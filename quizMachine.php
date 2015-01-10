e6521a394b4019fce7efa1c96ac9ed5771d85bf9
Stephen Jennings <kernrate17@gmail.com>
2015-01-10 13:57:45 -0800

    first
    Building array questions 16 each

---
+++ quizMachine.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * access remote repository on github with your account established
+ * use clone command: git clone https://github.com/your instance
+ * [master] is a repository
+ * [master red letters] means file ready to commit
+ * commit and turns green
+ *  push
+ *  git status
+ * git add before get commit is staging
+ *  after push you will see them on git
+ * github is a collection of repositories, good for collaboration, called social coding
+ *  octoDel
+ *  forking a repository is copying a repository from the githum site
+ *  Time: 3:16 PM
+ *
+ */
+
+
+$questions = array(
+
+array("Generally version control can be historically be divided into how may types? These type have evolved chronologically?", 100),
+    array("What is the primary objective of Version Control Systems?", 101),
+    array("What are two ways that VCS systems can differ from each other?", 102),
+array("What are the differences between the way that the three VCS's deal with storing files?", 103),
+array("What is the difference between file operations and fileset operations?", 103),
+array("If a third generation VCS is used, what sets it apart from early VCS's?", 104),
+array("Can a third generation VCS have multiple repositories?", 105),
+    array("Can a third generation VCS have multiple repositories?", 105),array("What is the acronym for Git Hubs system?", 106, DVCS),
+    array("What language is Git Hub written in?", 107, c and perl),array("What are the five action components of a Git DVCS (system)?", 108,Branch, Merge, Commit, Push, Pull ),
+    array("What is the purpose of the Git command init?", 109, create, a, repository  ),array("What is the purpose of the Git command status ?", 110, current, state, of, repository  ), array("What is the purpose of the Git command add?", 111, Add, files, to, the, repository),
+    array("What is the purpose of the Git command commit ?", 112, commit, current, working, directory  ),array("What is the purpose of the Git command log?", 113, see, changes ),array("What is the purpose of the Git command push?", 114, push to central repository ),array("What is the purpose of the Git command pull?", 115, pull, from, repository ), array("What is the purpose of the Git command merge ?", 116 ,merge, code, together  ),
+);
+
+?>
